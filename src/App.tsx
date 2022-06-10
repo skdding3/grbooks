2098b5e3ff6737147afe241dfd53b738187c5e13
skdding3 <skdding3@gmail.com>
2022-06-10 16:23:32 +0900

    router setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,31 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Signin from "./pages/Signin";
+import Add from "./pages/Add";
+import NotFound from "./pages/NotFound";
+import Detail from "./pages/Detail";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    // reactdom v6
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      {/* react router v6 */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/book/:id" element={<Detail />} />
+          {/* 책 추가 */}
+          <Route path="/add" element={<Add />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Home />} />
+          {/* 낫파운드 v6 적용 */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
